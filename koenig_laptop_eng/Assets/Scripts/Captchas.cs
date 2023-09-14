451ce3cc5d512160bafcc0a3fea90a02a8f4dd78
Dev Buzzty <s.f.falser@gmail.com>
2023-09-14 22:37:29 +0200

    [REMOVE] Projects

--- koenig_laptop_eng/Assets/Scripts/Captchas.cs
+++
@@ -1,9 +0,0 @@
-
-[System.Serializable]
-public class Captchas
-{
-    public string Question;
-    public string[] Answers;
-    public int FirstCorrectAnswer;
-    public int SecondCorrectAnswer;
-}
